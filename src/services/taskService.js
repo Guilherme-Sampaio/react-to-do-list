2dfd6918e76cc4eabc89fc2d63eebf8d4930fcd4
Guilherme Sampaio <guilherme.sampaio@zallpy.com>
2021-11-03 21:13:10 -0300

    adding task creation and list

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,1 +1,29 @@
+const baseUrl = 'http://localhost:8080/'
+
+const headers = new Headers({
+  "Content-Type": "application/json",
+});
+
+const defaultRequest = {
+  headers: headers,
+  mode: 'cors',
+  cache: 'default',
+};
+
+export const findAllTasks = () => {
+  const request = {
+    ...defaultRequest,
+    method: 'GET',
+  }
+  return fetch( `${baseUrl}/task/all`, request).then(response => response.json());
+}
+
+export function saveTask(task) {
+  const request = {
+    ...defaultRequest,
+    method: 'POST',
+    body: JSON.stringify(task),
+  };
+  return fetch(`http://localhost:8080/task`, request).then(response => response.json());
+}
 
